1d52c0610c5e67e37badb9b9bc3409fd86ec4c6b
markovjestica <m.vjestica94@gmail.com>
2017-02-10 02:56:19 +0100

    Expanded train set and added code for small images

--- Smart Dialer Python Engine/knn_from_drawing.py
+++ Smart Dialer Python Engine/knn_from_drawing.py
@@ -18,30 +18,41 @@ def img_resize(obj_img):
     return cv2.resize(res2, (img_x, img_y), interpolation=cv2.INTER_CUBIC)
 
 #size of characters
-img_x = 30
-img_y = 45
+img_x = 25
+img_y = 35
 
 #images for knn training, 40 numbers for all
-images = ["zero_training.jpeg", "zero_training2.jpeg", "one_training.jpeg", "one_training2.jpeg", "two_training.jpeg",
-          "two_training2.jpeg", "three_training.jpeg", "three_training2.jpeg", "four_training.jpeg",
-          "four_training2.jpg",
-          "five_training.jpeg", "five_training2.jpg", "six_training.jpeg", "six_training2.jpeg", "seven_training.jpeg",
-          "seven_training2.jpg", "eight_training.jpeg", "eight_training2.jpg", "nine_training.jpeg",
-          "nine_training2.jpeg",
-          "plus_training.jpeg", "plus_training2.jpeg", "slash_training.jpeg", "slash_training2.jpeg"]
+images = ["zero_training.jpeg", "zero_training2.jpeg", "zero_training3.jpeg",
+          "one_training.jpeg", "one_training2.jpeg", "one_training3.jpeg",
+          "two_training.jpeg", "two_training2.jpeg", "two_training3.jpeg",
+          "three_training.jpeg", "three_training2.jpeg", "three_training3.jpeg",
+          "four_training.jpeg", "four_training2.jpg", "four_training3.jpeg",
+          "five_training.jpeg", "five_training2.jpg", "five_training3.jpeg",
+          "six_training.jpeg", "six_training2.jpeg", "six_training3.jpeg",
+          "seven_training.jpeg", "seven_training2.jpg", "seven_training3.jpeg",
+          "eight_training.jpeg", "eight_training2.jpg", "eight_training3.jpeg",
+          "nine_training.jpeg", "nine_training2.jpeg", "nine_training3.jpeg",
+          "plus_training.jpeg", "plus_training2.jpeg", "plus_training3.jpeg",
+          "slash_training.jpeg", "slash_training2.jpeg", "slash_training3.jpeg"]
 train_array = np.zeros((len(images) * 20, img_x*img_y), np.float32)
 i = 0
+brojac = 1
 for img in images:
     one = cv2.imread("images/" + img)
     print(img)
     gray_img = 255 - cv2.cvtColor(one, cv2.COLOR_BGR2GRAY)
     gray_original = gray_img.copy()
-    ret, thresh_img = cv2.threshold(gray_img, 127, 255, 0)
+    ret, thresh_img = cv2.threshold(gray_img, 250, 255, 0)
     kernel = np.ones((10, 10), np.uint8)
     thresh = cv2.dilate(thresh_img, kernel, iterations=1)
 
     _, contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    #show_image(one, img, contours)
+
+    #if brojac == 41:
+        #show_image(one, img, contours)
+
+    if brojac == 61:
+        brojac = 1
 
     for cont in contours:
         num = cont.copy()
@@ -56,13 +67,17 @@ for img in images:
 
             train_array[i] = resized.reshape(-1, img_x*img_y).astype(np.float32)
 
+            a, b = img.split('_')
+            cv2.imwrite("images/im/" + a + "/" + str(brojac) + ".jpg", resized)
+
             i = i + 1
+            brojac = brojac + 1
 
 print train_array.shape
 
 #labels for knn training
 num_labels = np.arange(12)          #10='+'  11='/'   12='-'
-train_labels = np.repeat(num_labels, 40)[:, np.newaxis]
+train_labels = np.repeat(num_labels, 60)[:, np.newaxis]
 print train_labels.shape
 
 #knn training
@@ -73,7 +88,7 @@ knn.train(train_array, cv2.ml.ROW_SAMPLE, train_labels)
 test_img1 = cv2.imread("images/test5.jpeg")
 gray_test = 255 - cv2.cvtColor(test_img1, cv2.COLOR_BGR2GRAY)
 gray_test_original = gray_test.copy()
-retVal, thresh_test = cv2.threshold(gray_test, 127, 255, 0)
+retVal, thresh_test = cv2.threshold(gray_test, 250, 255, 0)
 kernel = np.ones((10, 10), np.uint8)
 thresh_test_img = cv2.dilate(thresh_test, kernel, iterations=1)
 
